b0c920593a22374c44888038ae8fc71efbcd5b45
jkesh <jkeshi@gmail.com>
2022-10-12 09:33:58 -0400

    my family part two

---
+++ exercise/objectScripts/array scripts/arrayscripts/my_family.js
@@ -0,0 +1,11 @@
+let myScores = [92, 98, 84, 76, 89, 99, 100];
+let yourScores = [82, 98, 94, 88, 92, 100, 100];
+
+function getAverage(scores) {
+    for (let i = 0; i < scores.length; i++) {
+        console.log(scores[i]);
+        
+    }
+}
+
+getAverage(myScores);
